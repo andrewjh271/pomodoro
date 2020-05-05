854862cd9fd7e38c77c5870f910d1b95a230b13d
Andrew Hayhurst <hayhurst.andrew@gmail.com>
2020-05-05 13:58:07 -0700

    Initial commit. Skeleton html

---
+++ script.js
@@ -0,0 +1,10 @@
+/* 
+let date = new Date(0);
+date.setSeconds(60 * 25);
+let timeString = date.toISOString().substr(11, 8);
+console.log(timeString)
+
+let timerOn = setInterval(function, milliseconds);
+clearInterval(timerOn);
+
+*/
