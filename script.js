2e4fb38046a1d106a70be11669bfca67f6ea89ff
Andrew Hayhurst <hayhurst.andrew@gmail.com>
2020-05-05 17:22:05 -0700

    Rough layout and timer functionality. After first pair programming session #1

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
-/* 
+let seconds = 60 * 1;
+
 let date = new Date(0);
-date.setSeconds(60 * 25);
-let timeString = date.toISOString().substr(11, 8);
-console.log(timeString)
+date.setSeconds(seconds);
+let timeString = prettifyDate();
+console.log(timeString);
 
-let timerOn = setInterval(function, milliseconds);
-clearInterval(timerOn);
+function prettifyDate() {
+  return date.toISOString().substr(14, 5)
+}
 
-*/
+function decreaseSecond() {
+  date.setSeconds(date.getSeconds() - 1);
+  if (date.getMinutes() === 0 &&
+    date.getSeconds() === 0) {
+    clearInterval(timerOn);
+  }
+  console.log(prettifyDate());
+  timer.textContent = prettifyDate();
+}
+
+let timerOn = setInterval(decreaseSecond, 1000);
+
+
+const timer = document.querySelector('#timer')
+const setSession = document.querySelector('#session')
+
+// setSession.textContent = 
+
