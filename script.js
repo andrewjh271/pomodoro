e853f259fce0aea905702d43ce79137cc59609e0
Andrew Hayhurst <hayhurst.andrew@gmail.com>
2020-05-08 16:24:59 -0700

    Most work done. CSS needs some updating and code needs a lot of cleaning up.

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ let timeString = prettifyDate();
 console.log(timeString);
 
 let timerOn;
+let timerCount = 0;
 
 const timer = document.querySelector('#timer');
 timer.textContent = prettifyDate();
@@ -29,6 +30,8 @@ let newSession = true;
 
 let startTime;
 
+// let gradientTimer;
+
 const extra = document.querySelector('.extra');
 
 let currentSession = 0;
@@ -38,8 +41,8 @@ let currentSession = 0;
 // console.log(topDiamond);
 const rootElement = document.documentElement;
 // topDiamond.style = 'background-image: linear-gradient(-45deg, #762ada, #13d4e2 70%, #762ada 80%)';
-console.log(getComputedStyle(rootElement).getPropertyValue('--linear-background-top'));
-rootElement.style.setProperty('--linear-background-top', 'linear-gradient(-45deg, #762ada, #13d4e2 100%, #762ada 80%)');
+// console.log(getComputedStyle(rootElement).getPropertyValue('--linear-background-top'));
+// rootElement.style.setProperty('--linear-background-top', 'linear-gradient(-45deg, #762ada, #13d4e2 100%, #762ada 80%)');
 
 session = [];
 session[0] = document.querySelector('.session0');
@@ -72,65 +75,79 @@ function buttonClick(e) {
       if(!extraTime) {
         if(!timerOn) {
           if(newSession) {
+            resetGradient();
             startTime = +sessionTime.textContent;
             newSession = false;
           }
-          timerOn = setInterval(decreaseSecond, 1000);
+          // setTimeout(function() {timerOn = setInterval(decreaseSecond, 100)}, 900);
+          timerOn = setInterval(decreaseSecond, 100);
+          // gradientTimer = setInterval(updateGradient, 100)
           pausePlay.classList.remove('fa-play')
           pausePlay.classList.add('fa-pause')
-          if(currentlyWorking) sessionTitle.classList.add('active-title');
-          else breakTitle.classList.add('active-title');
+          if(currentlyWorking) sessionTitle.classList.add('active-session');
+          else breakTitle.classList.add('active-break');
           break;
         } else {
           clearInterval(timerOn);
           timerOn = null;
+          // clearInterval(gradientTimer);
           pausePlay.classList.remove('fa-pause')
           pausePlay.classList.add('fa-play')
-          if(currentlyWorking) sessionTitle.classList.remove('active-title');
-          else breakTitle.classList.remove('active-title');
+          if(currentlyWorking) sessionTitle.classList.remove('active-session');
+          else breakTitle.classList.remove('active-break');
           break;
         }
       } else {
-        if(currentlyWorking) {
-          seconds = 60 * +breakTime.textContent;
-          currentlyWorking = false;
-          pausePlay.classList.remove('fa-mug-hot');
-          sessionTitle.classList.remove('active-title');
-          breakTitle.classList.add('active-title');
-        } else {
-          seconds = 60 * +sessionTime.textContent;
-          currentlyWorking = true;
-          pausePlay.classList.remove('fa-pencil-alt');
-          sessionTitle.classList.add('active-title');
-          breakTitle.classList.remove('active-title');
-        }
         pausePlay.classList.add('fa-pause')
         pausePlay.style = ('color: gray');
         clearInterval(timerOn);
         date = new Date(0);
-        date.setSeconds(seconds);
-        timer.textContent = prettifyDate();
-        timerOn = setInterval(decreaseSecond, 1000);
         extraTime = false;
         extra.classList.add('extra-hidden');
+        if(currentlyWorking) {
+          seconds = 60 * +breakTime.textContent;
+          date.setSeconds(seconds);
+          timer.textContent = prettifyDate();
+          currentlyWorking = false;
+          pausePlay.classList.remove('fa-mug-hot');
+          sessionTitle.classList.remove('active-session');
+          breakTitle.classList.add('active-break');
+          setTimeout(function() {timerOn = setInterval(decreaseSecond, 100)}, 900);
+        } else {
+          seconds = 60 * +sessionTime.textContent;
+          date.setSeconds(seconds);
+          timer.textContent = prettifyDate();
+          resetGradient();
+          // gradientTimer = setInterval(updateGradient, 100)
+          startTime = +sessionTime.textContent;
+          currentlyWorking = true;
+          pausePlay.classList.remove('fa-pencil-alt');
+          sessionTitle.classList.add('active-session');
+          breakTitle.classList.remove('active-break');
+          setTimeout(function() {timerOn = setInterval(decreaseSecond, 100)}, 900);
+        }
       }
       break;
     case('reset'):
+      resetGradient();
+      newSession = true;
       clearInterval(timerOn);
       timerOn = null;
+      // clearInterval(gradientTimer);
       seconds = 60 * +sessionTime.textContent;
       date = new Date(0);
       date.setSeconds(seconds);
       timer.textContent = prettifyDate();
       currentlyWorking = true;
+      currentSession = 0;
       extraTime = false;
       extra.classList.add('extra-hidden');
       session.forEach((session) => {
         session.classList.remove('fas');
         session.classList.add('far');
       })
-      sessionTitle.classList.remove('active-title');
-      breakTitle.classList.remove('active-title');
+      sessionTitle.classList.remove('active-session');
+      breakTitle.classList.remove('active-break');
       pausePlay.classList.remove('fa-pause')
       pausePlay.classList.remove('fa-pencil-alt');
       pausePlay.classList.remove('fa-mug-hot');
@@ -142,11 +159,16 @@ function buttonClick(e) {
   // date.setSeconds(seconds);
   // timer.textContent = prettifyDate();
 }
-
+// function incrementDate() {
+//   if(timerCount == 10) {
+//     timerCount = 0;
+//     decreaseSecond();
+//   } else timerCount++;
+//   updateGradient();
+// }
 function decreaseSecond() {
-  date.setSeconds(date.getSeconds() - 1);
-  if (date.getMinutes() === 0 &&
-    date.getSeconds() === 0) {
+  date.setMilliseconds(date.getMilliseconds() - 100);
+  if (date.getMinutes() === 0 && date.getSeconds() === 0) {
     if(currentlyWorking) {
       updateSessionCount();
       pausePlay.classList.remove('fa-pause');
@@ -156,13 +178,14 @@ function decreaseSecond() {
       pausePlay.classList.remove('fa-pause');
       pausePlay.classList.add('fa-pencil-alt');
     }
+    // clearInterval(gradientTimer);
     clearInterval(timerOn);
     timerOn = setInterval(increaseSecond, 1000);
     extraTime = true;
     extra.classList.remove('extra-hidden');
     extra.textContent = `Extra Time: ${prettifyDate()}`;
   }
-  updateGradient();
+  if(currentlyWorking) updateGradient();
   timer.textContent = prettifyDate();
 }
 function increaseSecond() {
@@ -187,9 +210,9 @@ function clearSessionCount() {
   })
 }
 function updateGradient() {
-  const remainingTime = date.getMinutes() * 60 + date.getSeconds();
-  console.log(remainingTime);
-  let progress = 1 - (remainingTime / (startTime * 60));
+  const remainingTime = date.getMinutes() * 60000 + date.getSeconds() * 1000 + date.getMilliseconds();
+  // console.log(remainingTime);
+  let progress = 1 - (remainingTime / (startTime * 60 * 1000));
   console.log(progress);
 
   let colorTwoStart = progress * 2 * 100;
@@ -200,9 +223,30 @@ function updateGradient() {
 
   let colorThreeStart = 60 + progress * 40;
 
-  console.log(colorTwoStart)
-  console.log(colorThreeStart)
+  let colorVariable = progress * 1.1 * 100;
+  console.log(colorVariable);
 
-  rootElement.style.setProperty('--linear-background-top', `linear-gradient(225deg,#13d4e2, #8672a1 ${colorTwoStart}%, #13d4e2 ${colorThreeStart}%)`);
-  rootElement.style.setProperty('--linear-background-bottom', `linear-gradient(-45deg, #762ada, #13d4e2 ${colorTwoStart}%, #762ada ${colorThreeStart}%`);
+  // console.log(colorTwoStart)
+  // console.log(colorThreeStart)
+
+  // rootElement.style.setProperty('--linear-background-top',
+  //   `linear-gradient(225deg,#13d4e2, #8672a1 ${colorTwoStart}%, #13d4e2 ${colorThreeStart}%)`);
+  // rootElement.style.setProperty('--linear-background-bottom', 
+  //   `linear-gradient(-45deg, #762ada, #13d4e2 ${colorTwoStart}%, #762ada ${colorThreeStart}%`);
+
+  rootElement.style.setProperty('--linear-background-top',
+    `linear-gradient(225deg,red, blue ${colorTwoStart}%, red ${colorThreeStart}%)`);
+
+
+  rootElement.style.setProperty('--linear-background-top',
+    `linear-gradient(222deg, #02ddec -70%, #de1af8 ${colorVariable}%, #02ddec 120%)`);
+  rootElement.style.setProperty('--linear-background-bottom', 
+  `linear-gradient(142deg, #02ddec -30%, #de1af8 ${colorVariable}%, #02ddec 170%)`);
+  
+}
+function resetGradient() {
+  rootElement.style.setProperty('--linear-background-top',
+    `linear-gradient(222deg, #02ddec -70%, #de1af8 0%, #02ddec 120%)`);
+  rootElement.style.setProperty('--linear-background-bottom', 
+    `linear-gradient(142deg, #02ddec -30%, #de1af8 0%, #02ddec 170%)`);
 }
