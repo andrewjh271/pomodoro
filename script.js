dd4ee9bd973c31e0021a3c43582b9ebea1a53f0f
Andrew Hayhurst <hayhurst.andrew@gmail.com>
2020-05-19 14:25:28 -0700

    Undid calc changes in CSS for --vh

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-let vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-window.addEventListener('resize', () => {
-  vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
-
 const sessionTime = document.querySelector('#set-session');
 const breakTime = document.querySelector('#set-break');
 
