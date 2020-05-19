67351f403427883b8e9b7ae2a9d207401732dcfd
Andrew Hayhurst <hayhurst.andrew@gmail.com>
2020-05-18 18:00:33 -0700

    Timing is now based on Date objects rather than setInterval. Now the timer will persist even when tab or window is not in focus. Still need to add functionality with Pause button (add time elapsed during Pause to targetTime)

--- script-scratch.js
+++ script-scratch.js
@@ -1,17 +1,19 @@
+// I wanted to move away from relying on setInterval to update the timer dispaly and gradient, since it seems that it stops updating when the browser tab or window have not been in focus for a while. The basic idea was subtracting two Dates, and getting either the time remaining or percent completed that way, which did basically work. However, this was making the timer appear to skip, because I was still using a setInterval to update the timer (it was now getting it's value from the two Dates, so would have persisted regardless of whether the browser tab was focused), which was offset from the milliseconds of the Date subtraction function. I broke this iteration of the program experimenting with getting rid of the setInterval altogether and changing it into a boolean that checked if milliseconds == 0, and updated the timer display if so. I think it would work, but I would have to change a LOT of code. Plus I still needed to keep track of the time elapsed during a pause, and account for that as well. I think if I wanted to get this working right the best thing would be to basically start from scratch, which right now I think is not in my best interest. This was a Pair Programming project (even though the vast majority was just me), and the code is extra chaotic because of that.
+
+
 const sessionTime = document.querySelector('#set-session');
 const breakTime = document.querySelector('#set-break');
 
 const sessionTitle = document.querySelector('#session-title');
 const breakTitle = document.querySelector('#break-title');
 
-sessionTime.textContent = 1;
-breakTime.textContent = 1;
+sessionTime.textContent = 25;
+breakTime.textContent = 5;
 
 buttons = document.querySelectorAll('button')
 buttons.forEach((button) => button.addEventListener('click', buttonClick));
 pausePlay = document.querySelector('i');
 
-let minutes;
 // let minutes = sessionTime.textContent;
 // let targetTime = new Date(0);
 // targetTime.setMinutes(minutes);
@@ -68,16 +70,16 @@ function buttonClick(e) {
       if(!extraTime) {
         if(!timerOn) {
           if(newSession) {
-            minutes = sessionTime.textContent;
+            let minutes = sessionTime.textContent;
             targetTime = new Date(0);
             targetTime.setMinutes(minutes);
             startTime = new Date();
             resetGradient();
             newSession = false;
           }
-          timerOn = setInterval(decreaseSecond, 1000)
-          if(currentlyWorking)
-            timerGradient = setInterval(updateGradient, 100);
+          timerOn = 'session';
+          calculate_time_session(timerOn);
+          timerGradient = setInterval(updateGradient, 100);
 
 
           pausePlay.classList.remove('fa-play')
@@ -88,7 +90,6 @@ function buttonClick(e) {
         } else {
           clearInterval(timerOn);
           timerOn = null;
-          clearInterval(timerGradient)
           pausePlay.classList.remove('fa-pause')
           pausePlay.classList.add('fa-play')
           if(currentlyWorking) sessionTitle.classList.remove('active-session');
@@ -99,32 +100,29 @@ function buttonClick(e) {
         pausePlay.classList.add('fa-pause')
         pausePlay.style = ('color: #1f91a4');
         clearInterval(timerOn);
+        date = new Date(0);
         extraTime = false;
         extra.classList.add('extra-hidden');
         if(currentlyWorking) {
-          minutes = +breakTime.textContent;
-          targetTime = new Date(0);
-          targetTime.setMinutes(minutes);
-          startTime = new Date();
+          seconds = 60 * +breakTime.textContent;
+          date.setSeconds(seconds);
+          timer.textContent = prettifyDate();
           currentlyWorking = false;
           pausePlay.classList.remove('fa-mug-hot');
           sessionTitle.classList.remove('active-session');
           breakTitle.classList.add('active-break');
-          timer.textContent = prettifyDate(targetTime);
-          timerOn = setInterval(decreaseSecond, 1000);
+          timerOn = setInterval(decreaseSecond, 100);
         } else {
-          minutes = +sessionTime.textContent;
-          targetTime = new Date(0);
-          targetTime.setMinutes(minutes);
-          startTime = new Date();
+          seconds = 60 * +sessionTime.textContent;
+          date.setSeconds(seconds);
+          timer.textContent = prettifyDate();
+          resetGradient();
+          startTime = +sessionTime.textContent;
           currentlyWorking = true;
           pausePlay.classList.remove('fa-pencil-alt');
           sessionTitle.classList.add('active-session');
           breakTitle.classList.remove('active-break');
-          resetGradient();
-          timer.textContent = prettifyDate(targetTime);
-          timerGradient = setInterval(updateGradient, 100)
-          timerOn = setInterval(decreaseSecond, 1000);
+          timerOn = setInterval(decreaseSecond, 100);
         }
       }
       break;
@@ -134,9 +132,9 @@ function buttonClick(e) {
       clearInterval(timerOn);
       timerOn = null;
       minutes = +sessionTime.textContent;
-      targetTime = new Date(0);
-      targetTime.setMinutes(minutes);
-      timer.textContent = prettifyDate(targetTime);
+      timerDate = new Date(0);
+      timerDate.setMinutes(minutes);
+      timer.textContent = prettifyDate(timerDate);
       currentlyWorking = true;
       currentSession = 0;
       extraTime = false;
@@ -156,7 +154,7 @@ function buttonClick(e) {
   }
 }
 function decreaseSecond() {
-  if (calculate_time_session() == "00:00") {
+  if (timerDate.getMinutes() === 0 && timerDate.getSeconds() === 0) {
     if(currentlyWorking) {
       updateSessionCount();
       pausePlay.classList.remove('fa-pause');
@@ -167,19 +165,19 @@ function decreaseSecond() {
       pausePlay.classList.add('fa-pencil-alt');
       pausePlay.style = ('color: rgb(208, 123, 15)')
     }
-    startTime = new Date();
-    extra.classList.remove('extra-hidden');
-    extra.textContent = `Extra Time: ${calculate_extra_time()}`;
     clearInterval(timerOn);
     clearInterval(timerGradient);
+    startTime = new Date(0);
     timerOn = setInterval(increaseSecond, 1000);
     extraTime = true;
+    extra.classList.remove('extra-hidden');
+    extra.textContent = `Extra Time: ${prettifyDate(calculate_time_break())}`;
     timer.textContent = '00:00';
   }
-  else timer.textContent = calculate_time_session();
+  else timer.textContent = prettifyDate(calculate_time_session('timer'));
 }
 function increaseSecond() {
-  extra.textContent = `Extra Time: ${calculate_extra_time()}`;
+  extra.textContent = `Extra Time: ${prettifyDate(calculate_time_break())}`;
 }
 function prettifyDate(date) {
   return date.toISOString().substr(14, 5)
@@ -198,41 +196,41 @@ function clearSessionCount() {
     session.classList.add('far');
   })
 }
-function resetGradient() {
-  rootElement.style.setProperty('--linear-background-top',
-    `linear-gradient(222deg, #02ddec -70%, #ee05db 0%, #02ddec 120%)`);
-  rootElement.style.setProperty('--linear-background-bottom', 
-    `linear-gradient(142deg, #02ddec -30%, #ee05db 0%, #02ddec 120%)`);
-}
-function updateGradient() {
-  currentTime = new Date();
-
-
-  let progress = currentTime.getTime() - startTime.getTime();
-  let progressPercent = progress / targetTime.getTime() * 100;
-
+function updateGradient(progressPercent) {
   colorVariable = progressPercent;
   rootElement.style.setProperty('--linear-background-top',
     `linear-gradient(222deg, #02ddec -70%, #ee05db ${colorVariable}%, #02ddec 120%)`);
   rootElement.style.setProperty('--linear-background-bottom', 
   `linear-gradient(142deg, #02ddec -30%, #ee05db ${colorVariable * 1.2}%, #02ddec 120%)`);  
 }
-function calculate_time_session() {
-  currentTime = new Date();
-
-
-  let progress = currentTime.getTime() - startTime.getTime();
-
-  let timerDate = new Date(0);
-  timerDate.setSeconds(Math.round((targetTime.getTime() - progress) / 1000));
-  console.log(timerDate)
-  console.log(timerDate.getMilliseconds());
-  return prettifyDate(timerDate)
+function resetGradient() {
+  rootElement.style.setProperty('--linear-background-top',
+    `linear-gradient(222deg, #02ddec -70%, #ee05db 0%, #02ddec 120%)`);
+  rootElement.style.setProperty('--linear-background-bottom', 
+    `linear-gradient(142deg, #02ddec -30%, #ee05db 0%, #02ddec 120%)`);
 }
-function calculate_extra_time() {
+function calculate_time_session (timerOn) {
+  while(timerOn = 'session') {
+    currentTime = new Date();
+
+
+    let progress = currentTime.getTime() - startTime.getTime();
+    let progressPercent = progress / targetTime.getTime() * 100;
+    console.log(progressPercent + '%');
+
+    timerDate = new Date(0);
+    timerDate.setMilliseconds(targetTime.getTime() - progress);
+
+    if(timerDate.getMilliseconds() == 0) {
+      timer.textContent = prettifyDate(timerDate)
+    }
+    updateGradient(progressPercent);
+  }
+}
+function calculate_time_break() {
   currentTime = new Date();
-  let elapsed_time = currentTime.getTime() - startTime.getTime();
-  elapsedDate = new Date(0);
-  elapsedDate.setSeconds(Math.round(elapsed_time / 1000))
-  return prettifyDate(elapsedDate);
+  let progress = currentTime.getTime() - startTime.getTime();
+  timerDate = new Date(0);
+  timerDate.setMilliseconds(progress);
+  return timerDate;
 }
