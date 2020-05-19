ef229ad32cfd61b5b89e1d817ef7126c81c3fd05
Andrew Hayhurst <hayhurst.andrew@gmail.com>
2020-05-19 15:08:09 -0700

    Updated for display on devices

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+window.addEventListener('resize', () => {
+  vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
 const sessionTime = document.querySelector('#set-session');
 const breakTime = document.querySelector('#set-break');
 
